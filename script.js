bcffa6400feedb31b6256692bd4cfa046134be94
arafat-ahmed-dev <arafatahmedshlbd194@gmail.com>
2024-05-23 23:48:32 +0600

    done

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const dropDowns= document.querySelectorAll(".drop-down select");
-const btn = document.querySelector("button")
-
-const apiUrl = 'https://v6.exchangerate-api.com/v6/7ab68b39e1c40d3f55545c85/latest/USD';
-
-
+const btn = document.querySelector("button");
+const fromCurrency = document.querySelector(".from select");
+const toCurrency = document.querySelector(".to select");
 
 for(let select of dropDowns){
     for (code in countryList){
@@ -17,27 +15,42 @@ for(let select of dropDowns){
         }
         select.append(option);
     }
-    console.log(select.value);
     if (select.name === from) {
-        console.log(`From is selected`);
     } else if (select.name === to) {
-        console.log(`From is selected`);
     }
     select.addEventListener("change", (evt)=> {
         updateFlag(evt.target);
     } )
 }
-
+window.addEventListener("load", ()=>{
+    getExchangeRate();
+});
 const updateFlag = async(element) =>{
-    let code = element.value;
-    console.log(code);
+    const code = element.value;
     let countrycode = countryList[code];
-    console.log(countrycode);
     let newsrc = `https://flagsapi.com/${countrycode}/flat/64.png`;
-    let img = document.querySelector("img");
+    let img = element.parentElement.querySelector("img");
     img.src= newsrc;
 };
-// let icon = document.querySelector("i");
-// // icon.addEventListener("click", ()=> {
-// //     select.name
-// // });
+
+function getExchangeRate() {
+    document.querySelector("button").addEventListener("click", async(evt)=> {
+        evt.preventDefault();
+        const amount = document.querySelector("form input");
+        let amountVal = amount.value;
+        if(amountVal == "" || amountVal == "0"){
+            amount.value = "1";
+            amountVal = 1;
+        }
+        const url = `https://v6.exchangerate-api.com/v6/7ab68b39e1c40d3f55545c85/latest/${fromCurrency.value}`;
+        try {
+            const response = await fetch(url);
+            const result = await response.json();
+            const exchangeRate = result.conversion_rates[toCurrency.value];
+            const totalAmount = amountVal * exchangeRate;
+            document.querySelector(".final-msg").innerText = `${amountVal} ${fromCurrency.value} = ${totalAmount} ${toCurrency.value}`;
+        } catch (error) {
+            console.error("Error fetching exchange rates:", error);
+        }
+    })
+}
